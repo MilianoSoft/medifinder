094e6b4e9c49eeddcf03f96d93f409b77aee8f6b
brangie miliano <branyig4@gmail.com>
2024-06-16 15:17:04 -0400

    modulo de paciente

--- views/dasboard/paciente.php
+++ views/dasboard/paciente.php
@@ -1,46 +1,87 @@
-<?php include_once __DIR__.'/headerDasboard.php';?>
+<?php include_once __DIR__ . '/headerDasboard.php'; ?>
 <!-- aqui ira el contenido -->
- <div class="contenedor">
-    <?php include_once __DIR__.'/../templates/alertas.php';?>
+<div class="contenedor">
+  <?php include_once __DIR__ . '/../templates/alertas.php'; ?>
 
-    <form class="campo-filtro">
-      
-        <div class="filtro">
-          <label>no expediente</label>
-          <input type="text" placeholder="numero de paciente">
-        </div>
+  <form class="campo-filtro">
 
-        <div class="filtro">
-          <label>nombre</label>
-          <input type="text" placeholder="numero de paciente">
-        </div>
+    <div class="filtro">
+      <label>no expediente</label>
+      <input type="text" placeholder="numero de paciente">
+    </div>
 
-        <div class="filtro">
-          <label>apellido</label>
-          <input type="text" placeholder="numero de paciente">
-        </div>
+    <div class="filtro">
+      <label>nombre</label>
+      <input type="text" placeholder="numero de paciente">
+    </div>
 
-        <div class="filtro">
-          <label>cedula</label>
-          <input type="text" placeholder="numero de paciente">
-        </div>
-       </form>
+    <div class="filtro">
+      <label>apellido</label>
+      <input type="text" placeholder="numero de paciente">
+    </div>
 
-    <div class="campo-botones">
-        <div class="campo-boton">
-        <div class="imagen-boton"><img class="img1" src="/img/icons/buscar.png" alt="imagen-buscar"></div>
-          <p>buscar paciente</p>
-        </div>
+    <div class="filtro">
+      <label>cedula</label>
+      <input type="text" placeholder="numero de paciente">
+    </div>
+  </form>
 
-        <div class="campo-boton" >
-        <div class="imagen-boton" ><img class="img1" src="/img/icons/usuario-editar.png" alt="imagen-buscar"></div>
-          <p>Editar paciente</p>
-        </div>
-        <div class="campo-boton">
-        <div class="imagen-boton"><img class="img1" src="/img/icons/agregar-usuario.png" alt="imagen-buscar"></div>
-          <p>nuevo paciente</p>
-        </div>
-    </div> <!-- fin de botones -->
- </div>
+  <div class="campo-botones">
+    <div class="campo-boton">
+      <div class="imagen-boton"><img class="img1" src="/img/icons/buscar.png" alt="imagen-buscar"></div>
+      <p>buscar paciente</p>
+    </div>
+    <div class="campo-boton" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      <div class="imagen-boton"><img class="img1" src="/img/icons/agregar-usuario.png" alt="imagen-buscar"></div>
+      <p>nuevo paciente</p>
+    </div>
+  </div> <!-- fin de botones -->
+
+  <div class="campo-tabla">
+    <table class="table">
+      <thead class="table-info table-head">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">No Exp</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Apellido</th>
+          <th scope="col">Cedula</th>
+          <th scope="col">Sexo</th>
+          <th scope="col">Fecha Naci</th>
+          <th scope="col">acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>0001</td>
+          <td>onesimo</td>
+          <td>coronado</td>
+          <td>402-1175-126-4</td>
+          <td>masculino</td>
+          <td>04/09/1998</td>
+          <td class="tabla-acciones">
+                <div class="tabla-boton"><img class="img1" src="/img/icons/usuario-editar.png" alt="imagen-buscar">editar</div>
+                <div class="tabla-boton"><img class="img1" src="/img/icons/borrar-usuario.png" alt="imagen-buscar">eliminar</div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div> <!-- tabla muestra los pacientes registrados -->
+
+  <div class=" rigth campo-botones">
+    <div class="campo-boton">
+      <div class="imagen-boton"><img class="img1" src="/img/icons/carpeta.png" alt="imagen-buscar"></div>
+      <p> historia clinica</p>
+    </div>
+    <div class="campo-boton">
+      <div class="imagen-boton"><img class="img1" src="/img/icons/consentimiento.png" alt="imagen-buscar"></div>
+      <p> consentimiento</p>
+    </div>
+  </div> <!-- fin de botones -->
+</div>
 <!-- fin del contenido -->
-<?php include_once __DIR__.'/footerDasboard.php';?>
+
+<?php include_once __DIR__. '/../templates/modal.php';?>
+
+<?php include_once __DIR__ . '/footerDasboard.php'; ?>
---
+++ views/templates/modal.php
@@ -0,0 +1,89 @@
+<!-- Button trigger modal -->
+<!-- 
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>  -->
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Registrar Nuevo Paciente</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+                <form class="perfil">
+                <fieldset> 
+                        <div class="foto">
+                            <img class="img1" src="/img/icons/sin-foto.png" alt="foto-imagen">
+                        </div>
+                        <div class="boton-camara">
+                            <img class="img1" src="/img/icons/camara.png" alt="foto-imagen">
+                            <input type="file" aria-label="subir foto">
+                        </div>
+                        <div>
+                            <label>No Expediente</label>
+                            <input type="text">
+                        </div>
+                        <div>
+                            <label>Nombre</label>
+                            <input type="text">
+                        </div>
+                    </fieldset>
+                    <fieldset>
+
+                        <div>
+                            <label>Apellido</label>
+                            <input type="text">
+                        </div>
+                        <div>
+                            <label>Telefono</label>
+                            <input type="text">
+                        </div>
+
+                        <div>
+                            <label>Cedula</label>
+                            <input type="text">
+                        </div>
+                        <div>
+                            <label>correo electronico</label>
+                            <input type="text">
+                        </div>
+                        <div>
+                            <label>sexo</label>
+                            <input type="text">
+                        </div>
+                    </fieldset>
+                    <fieldset>
+                        <div>
+                            <label>fecha de nacimiento</label>
+                            <input type="text">
+                        </div>
+                        <div>
+                            <label>pais</label>
+                            <input type="text">
+                        </div>
+                        <div>
+                            <label>ciudad</label>
+                            <input type="text">
+                        </div>
+                        <div>
+                            <label>direccion</label>
+                            <textarea name="" id="" rows="5">
+
+                            </textarea>
+                        </div>
+                    </fieldset>
+                    </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Descartar</button>
+                <button type="submit" class="btn btn-primary">Guardar registro</button>
+            </div>
+            
+        </div>
+    </div>
+</div>
+<!-- fin del modal -->
